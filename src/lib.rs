5a419af7b9d56834a101a6cf64a89eea1617167e
Ben Stern <bstern@kukaret.com>
2016-07-07 17:10:28 -0400

    Importing right before big signature changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
+#![crate_type = "lib"]
+#![crate_name = "websession"]
+
+extern crate time;
+extern crate uuid;
+extern crate pwhash;
+
 #[cfg(feature = "hyper")]
 extern crate hyper;
+
+use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::error::Error;
+use std::collections::HashMap;
+use uuid::Uuid;
+use pwhash::bcrypt;
 
 #[cfg(feature = "hyper")]
 use hyper::server::request::Request;
@@ -9,21 +25,56 @@ use hyper::server::response::Response;
 #[derive(Debug)]
 pub enum SessionError {
     Unauthorized,
+    IO(String),
 }
 
+// impl From<std::io::Error> for SessionError {
+//     fn from(err: std::io::Error) -> SessionError {
+//         SessionError::IO(err.description().to_string())
+//     }
+// }
+
+#[derive(Hash, Eq, PartialEq, Debug)]
 pub enum SessionPolicy {
     Simple,      // check username/pw, session id for expiration
     AddressLock, // Simple plus check sessionid against original ip address
+    AddressPortLock,
 }
 
+#[derive(Debug, Copy, Clone)]
 pub struct ConnectionSignature {
+    uuid: Option<Uuid>,
+    ipv4: Option<[u8; 4]>,
+    ipv6: Option<[u8; 16]>,
+    port: u16
     // signature details here
 }
 
+impl PartialEq for ConnectionSignature {
+    // We only compare by UUID for hashing; the library will
+    // need to compare the other components; this is how we can
+    // detect attempted collisions, since UUIDs are supposed to be
+    // unique.
+    fn eq(&self, other: &Self) -> bool { self.uuid == other.uuid }
+}
+
+impl Eq for ConnectionSignature {}
+
+use std::hash::Hash;
+use std::hash::Hasher;
+impl Hash for ConnectionSignature {
+    fn hash<H: Hasher>(&self, h: &mut H) { self.uuid.hash(h); }
+}
+
 impl ConnectionSignature {
-    pub fn new() -> ConnectionSignature {
+    pub fn new(ipv4: Option<[u8; 4]>, ipv6: Option<[u8; 16]>, port: u16) -> ConnectionSignature {
         // we may need a builder pattern here if this gets complicated
-        ConnectionSignature{}
+        ConnectionSignature {
+            uuid: Uuid::new_v4(),
+            ipv4: ipv4,
+            ipv6: ipv6,
+            port: port
+        }
     }
 
     #[cfg(feature = "hyper")]
@@ -33,23 +84,82 @@ impl ConnectionSignature {
     }
 }
 
-pub struct SessionManager {
+#[derive(Debug)]
+pub struct SessionManager<T> {
     expiration: u64, // in time (seconds) since last access
     policy: SessionPolicy,
-    dummy: Session, // for verifying API, Todo: replace with something like the line below
-    // sessions: HashMap<ConnectionSignature, Session> // - or something like this
+    backing_store: T,
+    cookie_dir: String,
+    sessions: HashMap<ConnectionSignature, Session>
 }
 
-impl SessionManager {
-    pub fn new(expiration: u64, policy: SessionPolicy) -> SessionManager {
-        SessionManager{expiration: expiration, policy: policy, dummy: Session{user: None, session_id: "42".to_string()}}
+impl <T: AsRef<Path>> SessionManager<T> {
+    pub fn new(expiration: u64, policy: SessionPolicy, backing_store: T) -> SessionManager<T> {
+        SessionManager {
+            expiration: expiration,
+            policy: policy,
+            backing_store: backing_store,
+            cookie_dir: "cookies".to_string(),
+            sessions: HashMap::new()
+        }
     }
 
     // if valid, sets session cookie in res and returns a Session
     // struct (or maybe a reference to a Session struct,
-    // i.e. Result<&Session, SessionError>)
-    pub fn login(&self, user: &str, password: &str, signature: ConnectionSignature) -> Result<&Session, SessionError> {
-        panic!("Not implemented!");
+    // i.e. Result<Session, SessionError>)
+    pub fn login(&mut self, user: String, password: &str,
+        signature: ConnectionSignature) -> Result<Uuid, SessionError> {
+        if user.trim() != user {
+            return Err(SessionError::Unauthorized);
+        }
+        if user.len() < 1 {
+            return Err(SessionError::Unauthorized);
+        }
+        if (self.policy == SessionPolicy::AddressLock) ||
+            (self.policy == SessionPolicy::AddressPortLock) {
+            if signature.ipv4.is_none() && signature.ipv6.is_none() {
+                return Err(SessionError::Unauthorized);
+            }
+        }
+        if self.policy == SessionPolicy::AddressPortLock {
+            if signature.port == 0 {
+                return Err(SessionError::Unauthorized);
+            }
+        }
+
+        let session = Session {
+            session_id: signature.uuid,
+            user: Some(user.to_string()),
+            ipv4: signature.ipv4,
+            ipv6: signature.ipv6,
+            port: signature.port,
+            last_access: time::now().to_timespec(),
+        };
+
+        let ref p = self.backing_store;
+        let f = match File::open(p) {
+            Err(x) => return Err(SessionError::IO(x.description().to_string())),
+            Ok(f) => f,
+        };
+        let reader = BufReader::new(f);
+        for line in reader.lines() {
+            let s = line.unwrap();
+            let v: Vec<&str> = s.split(':').collect();
+            // Format: username:bcrypt
+            if v[0] == user {
+                println!("Found user {}!", user);
+                let h = bcrypt::hash(password).unwrap();
+                if v[2] == h {
+                    // If this is a valid login, stomp on any outstanding
+                    // sessions matching this identifier
+                    self.sessions.insert(signature, session);
+                    return Ok(session);
+                } else {
+                    return Err(SessionError::Unauthorized);
+                }
+            }
+        }
+        return Err(SessionError::Unauthorized);
     }
 
     #[cfg(feature = "hyper")]
@@ -60,8 +170,29 @@ impl SessionManager {
 
     // if valid, returns the session struct and possibly update cookie in res
     pub fn get_session(&self, signature: ConnectionSignature) -> Result<&Session, SessionError> {
-        // panic!("Not implemented!");
-        Ok(&self.dummy)
+        let mut rv = Err(SessionError::Unauthorized);
+        if self.sessions.contains_key(&signature) {
+            let compare = self.sessions.get(&signature).unwrap();
+            match self.policy {
+                SessionPolicy::Simple => rv = Ok(compare),
+                SessionPolicy::AddressLock => if
+                    (signature.ipv4.is_some() && signature.ipv4.eq(&compare.ipv4)) ||
+                    (signature.ipv6.is_some() && signature.ipv6.eq(&compare.ipv6)) {
+                    self.sessions.get_mut(&signature).unwrap().last_access =
+                        time::now().to_timespec();
+                    rv = Ok(compare);
+                },
+                SessionPolicy::AddressPortLock => if
+                    ((signature.ipv4.is_some() && signature.ipv4.eq(&compare.ipv4)) ||
+                    (signature.ipv6.is_some() && signature.ipv6.eq(&compare.ipv6))) &&
+                    (signature.port == compare.port) && (signature.port > 0) {
+                    self.sessions.get_mut(&signature).unwrap().last_access =
+                        time::now().to_timespec();
+                    rv = Ok(compare);
+                }
+            }
+        }
+        rv
     }
 
     #[cfg(feature = "hyper")]
@@ -86,9 +217,15 @@ impl SessionManager {
     }
 }
 
+#[derive(Debug)]
 pub struct Session {
-    user: Option<String>,
-    session_id: String,
+    session_id: Uuid,
+    // vars: HashMap<String, String>,
+    user: Option<String>, // this isn't in the hashmap because all sessions have a user
+    ipv4: Option<[u8; 4]>,
+    ipv6: Option<[u8; 16]>,
+    port: u16,
+    last_access: time::Timespec,
 }
 
 impl Session {
@@ -104,9 +241,8 @@ impl Session {
         &self.user
     }
 
-    pub fn get_session_id(&self) -> &String {
-        // panic!{"Not implemented"};
-        &self.session_id
+    pub fn get_session_id(self) -> String {
+        return self.session_id.to_string();
     }
 
     // Session data methods
