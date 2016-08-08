face086a04a28bac672b3c4a5a8ccefb2e02210d
Ben Stern <bstern@kukaret.com>
2016-08-08 17:44:40 -0400

    Dike out underspecified methods

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,8 @@ use std::error::Error;
 use std::collections::HashMap;
 use uuid::Uuid;
 use pwhash::bcrypt;
-use std::net::SocketAddr;
-use std::net::IpAddr;
+// use std::net::SocketAddr;
+// use std::net::IpAddr;
 
 #[cfg(feature = "hyper")]
 use hyper::server::request::Request;
@@ -104,6 +104,7 @@ impl <T: AsRef<Path>> SessionManager<T> {
         }
     }
 
+/*
     fn valid_policy(&self, signature: &ConnectionSignature) -> bool {
         // everything in the match goes away when
         // https://github.com/rust-lang/rust/pull/34694 makes it to stable
@@ -123,16 +124,22 @@ impl <T: AsRef<Path>> SessionManager<T> {
         // }
         true
     }
+*/
 
-			// XXX we should mutate the logged in session
+// It's always okay to log out an expired session, so don't both
     // if valid, sets session cookie in res and returns a Session
     pub fn login(&mut self, user: String, password: &str,
         session: &Session) -> Result<&Session, SessionError> {
-	let now = time::now().to_timespec();
 	let sig = session.signature;
 
+	// This doesn't validate the signature against the session, nor does it
+	// make sure the signature matches our policy requirements.  Right now,
+	// it's pretty much impossible for either of these to fail, because we
+	// don't have any requirements.
+
 	let remove = match self.sessions.get_mut(&sig.uuid) {
-	    Some(cs) => if (now - cs.last_access) > self.expiration {
+	    Some(cs) => if (time::now().to_timespec() - session.last_access) >=
+		self.expiration {
 		true
 	    } else {
 		cs.update_access();
@@ -169,6 +176,12 @@ impl <T: AsRef<Path>> SessionManager<T> {
 	return Err(SessionError::Unauthorized);
     }
 
+    pub fn logout(&mut self, session: &Session) {
+	// Let's blow away the session and let them make a new anonymous session
+	// on next login.
+	self.sessions.remove(&session.signature.uuid);
+    }
+
     #[cfg(feature = "hyper")]
     pub fn login_hyper(&self, user: &str, password: &str, req: &Request) -> Result<&Session, SessionError> {
         let conn = ConnectionSignature::new_hyper(req);
@@ -178,14 +191,12 @@ impl <T: AsRef<Path>> SessionManager<T> {
     // if valid, returns the session struct and possibly update cookie in
     // res; if invalid, returns None
     pub fn start(self: &mut Self, signature: ConnectionSignature) -> Result<&Session, SessionError> {
-	let now = time::now().to_timespec();
-
 	// We now think the hash has to contain our session.  Let's see if it
 	// conforms to our signature.
 	let need_replacement = match self.sessions.get_mut(&signature.uuid) {
 	    None => true,
 	    Some(sess) => if sess.match_signature(&signature) {
-		if (now - sess.last_access) <= self.expiration {
+		if (time::now().to_timespec() - sess.last_access) <= self.expiration {
 	    	    sess.update_access();
     		    false
 		} else {
@@ -216,11 +227,6 @@ impl <T: AsRef<Path>> SessionManager<T> {
     // Todo: Nickel does not give us direct access to a hyper response
     // object. We need to figure out a clean way of setting the
     // cookie, ideally w/o requiring Nickel to be compiled in.
-
-    // logout the user associated with this session
-    pub fn logout_session(&self, session: Session) {
-        panic!{"Not implemented"};
-    }
 
     // logout all sessions
     pub fn logout_all_sessions(&mut self) {
@@ -281,9 +287,10 @@ impl Session {
     }
 
     pub fn get_session_id(self) -> String {
-        return self.signature.uuid.to_string();
+        self.signature.uuid.to_string()
     }
 
+/*
     // Session data methods
     pub fn get_data(&self, key: &str) -> Result<Option<String>, SessionError> {
         panic!("Not implemented!");
@@ -303,11 +310,7 @@ impl Session {
     pub fn set_persistant_data(&self, key: &str, value: &str) -> Result<(), SessionError> {
         panic!("Not implemented!");
     }
-
-    // log the user out
-    pub fn logout(&self) {
-        panic!("not implemented");
-    }
+*/
 }
 
 #[cfg(test)]
