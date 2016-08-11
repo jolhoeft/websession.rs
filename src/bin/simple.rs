dfc70defe0a117c911a7db2f24c699dd56c6329f
Ben Stern <bstern@kukaret.com>
2016-08-11 16:12:28 -0400

    Rename for discussion purposes.

--- src/bin/simple.rs
+++ src/bin/simple.rs
@@ -7,14 +7,14 @@ use time::Duration;
 
 fn main() {
     let path = Path::new("../../data/passwd");
-    let sessions = SessionManager::new(Duration::seconds(3600),
+    let mut session_manager = SessionManager::new(Duration::seconds(3600),
 	SessionPolicy { }, path);
     let signature = ConnectionSignature::new();
-    let mut session = match sessions.start(signature) {
+    let mut session = match session_manager.start(signature) {
 	Ok(sess) => sess,
 	Err(err) => panic!(err),
     };
-    session = match sessions.login("user".to_string(), "password", session) {
+    session = match session_manager.login("user".to_string(), "password", session) {
 	Ok(sess) => sess,
 	Err(err) => panic!(err),
     };
