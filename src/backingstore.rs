ea5f5d9f13b2ad80237cc7e7473a6a2de0732baa
Ben Stern <bstern@kukaret.com>
2016-08-24 13:04:29 -0400

    Implement update_pwhash

--- src/backingstore.rs
+++ src/backingstore.rs
@@ -2,7 +2,7 @@ use std::io;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
+use std::io::{Read, Write, BufWriter};
 use std::convert::From;
 
 #[derive(Debug)]
@@ -10,6 +10,12 @@ pub enum BackingStoreError {
     NoSuchUser,
     MissingData,
     IO(io::Error),
+}
+
+impl From<io::Error> for BackingStoreError {
+    fn from(err: io::Error) -> BackingStoreError {
+	BackingStoreError::IO(err)
+    }
 }
 
 // The BackingStore doesn't know about userIDs vs usernames; the consumer of
@@ -38,35 +44,62 @@ impl FileBackingStore {
 	    filename: filename.to_string(),
 	}
     }
+
+    fn load_file(&self) -> Result<String, BackingStoreError> {
+	let mut f = try!(File::open(self.filename.clone()).map_err(BackingStoreError::IO));
+	let mut buf = String::new();
+	try!(f.read_to_string(&mut buf).map_err(BackingStoreError::IO));
+	Ok(buf)
+    }
+
+    fn line_has_user(&self, line: &str, user: &String) -> Result<Option<String>, BackingStoreError> {
+	let v: Vec<&str> = line.split(':').collect();
+	if v.len() < 1 {
+	    Err(BackingStoreError::MissingData)
+	} else if v[0] == user {
+	    if v.len() != 2 {
+		Err(BackingStoreError::MissingData)
+	    } else {
+		Ok(Some(v[1].to_string()))
+	    }
+	} else {
+	    Ok(None)
+	}
+    }
 }
 
 impl BackingStore for FileBackingStore {
     fn get_pwhash(&self, user: &String) -> Result<String, BackingStoreError> {
-	let f = try!(File::open(self.filename.clone()).map_err(BackingStoreError::IO));
-	let reader = BufReader::new(f);
-	for line in reader.lines() {
-	    match line {
-		Err(e) => return Err(BackingStoreError::IO(e)),
-		Ok(s) => {
-		    let v: Vec<&str> = s.split(':').collect();
-		    if v.len() < 1 {
-			return Err(BackingStoreError::MissingData);
-		    } else if v[0] == user {
-			if v.len() != 2 {
-			    return Err(BackingStoreError::MissingData);
-			}
-			println!("Found user {}!", user);
-			return Ok(v[1].to_string())
-		    }
-		},
+	let pwfile = try!(self.load_file());
+	for line in pwfile.lines() {
+	    match try!(self.line_has_user(line, user)) {
+		Some(hash) => return Ok(hash),
+		None => { }, // keep looking
 	    }
 	}
 	Err(BackingStoreError::NoSuchUser)
     }
 
-    // This is problematic because I can't find a stable mkstemp in Rust.
     fn update_pwhash(&mut self, user: &String, new_pwhash: &String) -> Result<(), BackingStoreError> {
-	panic!("Not implemented");
+	let pwfile = try!(self.load_file());
+
+	let mut newfn = self.filename.clone();
+	newfn.push_str(".old");
+	try!(fs::rename(self.filename.clone(), newfn));
+	let mut f = BufWriter::new(try!(File::create(self.filename.clone()).map_err(BackingStoreError::IO)));
+	for line in pwfile.lines() {
+	    match try!(self.line_has_user(line, user)) {
+		Some(hash) => {
+		    try!(f.write_all(user.as_bytes()).map_err(BackingStoreError::IO));
+		    try!(f.write_all(b":").map_err(BackingStoreError::IO));
+		    try!(f.write_all(new_pwhash.as_bytes()).map_err(BackingStoreError::IO));
+		},
+		None => try!(f.write_all(line.as_bytes()).map_err(BackingStoreError::IO)),
+	    }
+	    // Intentionally ignoring \r\n under Windows; we're the consumer
+	    try!(f.write_all(b"\n").map_err(BackingStoreError::IO));
+	}
+	Ok(())
     }
 
     // This is problematic because I can't find a stable mkstemp in Rust.
