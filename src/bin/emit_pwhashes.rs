0d1fb9673cc555160e28074d07b94c98dabad3ff
Ben Stern <bstern@kukaret.com>
2016-08-24 14:15:15 -0400

    A helper program.

---
+++ src/bin/emit_pwhashes.rs
@@ -0,0 +1,14 @@
+extern crate pwhash;
+
+use std::io;
+use std::io::BufRead;
+use pwhash::bcrypt;
+
+fn main() {
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+	let plaintext = line.unwrap();
+	let hash = bcrypt::hash(&plaintext).unwrap();
+	println!("The hash of {} is {}", plaintext, hash);
+    }
+}
