4177d58e914a0190175e8cc752dc4b8cc0523717
Ben Stern <bstern@kukaret.com>
2016-09-14 15:10:54 -0400

    Wrap the MemoryBackingStore in a mutex too!

--- src/backingstore.rs
+++ src/backingstore.rs
@@ -28,12 +28,12 @@ impl From<io::Error> for BackingStoreError {
 // N.B., implementors of BackingStore provide a new that gets whatever is needed
 // to connect to the store.
 pub trait BackingStore {
-    fn get_pwhash(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError>;
-    fn update_pwhash(&mut self, user: &str, new_pwhash: &str) -> Result<(), BackingStoreError>;
+    fn get_credentials(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError>;
+    fn update_credentials(&mut self, user: &str, new_creds: &str) -> Result<(), BackingStoreError>;
     fn lock(&mut self, user: &str) -> Result<(), BackingStoreError>;
     fn islocked(&self, user: &str) -> Result<bool, BackingStoreError>;
     fn unlock(&mut self, user: &str) -> Result<(), BackingStoreError>;
-    fn create(&mut self, user: &str, pwhash: &str) -> Result<(), BackingStoreError>;
+    fn create(&mut self, user: &str, creds: &str) -> Result<(), BackingStoreError>;
     fn delete(&mut self, user: &str) -> Result<(), BackingStoreError>;
 }
 
@@ -51,16 +51,12 @@ impl FileBackingStore {
     }
 
     fn load_file(&self) -> Result<String, BackingStoreError> {
-        match self.filename.lock() {
-            Ok(fname) => {
-                let name = fname.clone();
-                let mut f = try!(File::open(name));
-                let mut buf = String::new();
-                try!(f.read_to_string(&mut buf));
-                Ok(buf)
-            },
-            Err(_) => Err(BackingStoreError::Mutex),
-        }
+        let fname = try!(self.filename.lock().map_err(|_| BackingStoreError::Mutex));
+        let name = fname.clone();
+        let mut f = try!(File::open(name));
+        let mut buf = String::new();
+        try!(f.read_to_string(&mut buf));
+        Ok(buf)
     }
 
     fn line_has_user(&self, line: &str, user: &str, fail_if_locked: bool) -> Result<Option<String>, BackingStoreError> {
@@ -89,47 +85,43 @@ impl FileBackingStore {
     }
 
     // We're intentionally ignoring \r\n under Windows; we're the consumer too.
-    fn update_user_hash(&self, user: &str, new_pwhash: Option<&str>, fail_if_locked: bool) -> Result<(), BackingStoreError> {
+    fn update_user_hash(&self, user: &str, new_creds: Option<&str>, fail_if_locked: bool) -> Result<(), BackingStoreError> {
         let mut found = false;
         let pwfile = try!(self.load_file());
 
-        match self.filename.lock() {
-            Err(_) => Err(BackingStoreError::Mutex),
-            Ok(fname) => {
-                let oldfn = fname.clone();
-                let newfn = oldfn.to_string() + ".old";
-                try!(fs::rename(oldfn.clone(), newfn));
-                let mut f = BufWriter::new(try!(File::create(oldfn)));
-                for line in pwfile.lines() {
-                    match try!(self.line_has_user(line, user, fail_if_locked)) {
-                        Some(_) => match new_pwhash {
-                            Some(newhash) => {
-                                try!(f.write_all(user.as_bytes()));
-                                try!(f.write_all(b":"));
-                                try!(f.write_all(newhash.as_bytes()));
-                                try!(f.write_all(b"\n"));
-                                found = true;
-                            },
-                            None => found = true, // we're deleting, don't write out
-                        },
-                        None => { // no user on this line, continue
-                            try!(f.write_all(line.as_bytes()));
-                            try!(f.write_all(b"\n"));
-                        },
-                    }
-                }
-                if found {
-                    Ok(())
-                } else {
-                    Err(BackingStoreError::NoSuchUser)
-                }
+        let fname = try!(self.filename.lock().map_err(|_| BackingStoreError::Mutex));
+        let oldfn = fname.clone();
+        let newfn = oldfn.to_string() + ".old";
+        try!(fs::rename(oldfn.clone(), newfn));
+        let mut f = BufWriter::new(try!(File::create(oldfn)));
+        for line in pwfile.lines() {
+            match try!(self.line_has_user(line, user, fail_if_locked)) {
+                Some(_) => match new_creds {
+                    Some(newhash) => {
+                        try!(f.write_all(user.as_bytes()));
+                        try!(f.write_all(b":"));
+                        try!(f.write_all(newhash.as_bytes()));
+                        try!(f.write_all(b"\n"));
+                        found = true;
+                    },
+                    None => found = true, // we're deleting, don't write out
+                },
+                None => { // no user on this line, continue
+                    try!(f.write_all(line.as_bytes()));
+                    try!(f.write_all(b"\n"));
+                },
             }
+        }
+        if found {
+            Ok(())
+        } else {
+            Err(BackingStoreError::NoSuchUser)
         }
     }
 }
 
 impl BackingStore for FileBackingStore {
-    fn get_pwhash(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError> {
+    fn get_credentials(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError> {
         let pwfile = try!(self.load_file());
         for line in pwfile.lines() {
             match try!(self.line_has_user(line, user, fail_if_locked)) {
@@ -140,12 +132,12 @@ impl BackingStore for FileBackingStore {
         Err(BackingStoreError::NoSuchUser)
     }
 
-    fn update_pwhash(&mut self, user: &str, new_pwhash: &str) -> Result<(), BackingStoreError> {
-        self.update_user_hash(user, Some(new_pwhash), true)
+    fn update_credentials(&mut self, user: &str, new_creds: &str) -> Result<(), BackingStoreError> {
+        self.update_user_hash(user, Some(new_creds), true)
     }
 
     fn lock(&mut self, user: &str) -> Result<(), BackingStoreError> {
-        let mut hash = try!(self.get_pwhash(user, false));
+        let mut hash = try!(self.get_credentials(user, false));
         if !try!(self.hash_is_locked(&hash)) {
             hash.insert(0, '!');
             return self.update_user_hash(user, Some(&hash), false);
@@ -155,12 +147,12 @@ impl BackingStore for FileBackingStore {
     }
 
     fn islocked(&self, user: &str) -> Result<bool, BackingStoreError> {
-        let hash = try!(self.get_pwhash(user, false));
+        let hash = try!(self.get_credentials(user, false));
         self.hash_is_locked(&hash)
     }
 
     fn unlock(&mut self, user: &str) -> Result<(), BackingStoreError> {
-        let mut hash = try!(self.get_pwhash(user, false));
+        let mut hash = try!(self.get_credentials(user, false));
         if try!(self.hash_is_locked(&hash)) {
             hash.remove(0);
             return self.update_user_hash(user, Some(&hash), false);
@@ -169,20 +161,18 @@ impl BackingStore for FileBackingStore {
         Ok(())
     }
 
-    fn create(&mut self, user: &str, pwhash: &str) -> Result<(), BackingStoreError> {
-        match self.get_pwhash(user, false) {
+    fn create(&mut self, user: &str, creds: &str) -> Result<(), BackingStoreError> {
+        match self.get_credentials(user, false) {
             Ok(_) => Err(BackingStoreError::UserExists),
-            Err(BackingStoreError::NoSuchUser) => match self.filename.lock() {
-                Ok(fname) => {
-                    let name = (*fname).clone();
-                    let mut f = BufWriter::new(try!(OpenOptions::new().append(true).open(name)));
-                    try!(f.write_all(user.as_bytes()));
-                    try!(f.write_all(b":"));
-                    try!(f.write_all(pwhash.as_bytes()));
-                    try!(f.write_all(b"\n"));
-                    Ok(())
-                },
-                Err(_) => Err(BackingStoreError::Mutex),
+            Err(BackingStoreError::NoSuchUser) => {
+                let fname = try!(self.filename.lock().map_err(|_| BackingStoreError::Mutex));
+                let name = (*fname).clone();
+                let mut f = BufWriter::new(try!(OpenOptions::new().append(true).open(name)));
+                try!(f.write_all(user.as_bytes()));
+                try!(f.write_all(b":"));
+                try!(f.write_all(creds.as_bytes()));
+                try!(f.write_all(b"\n"));
+                Ok(())
             },
             Err(e) => Err(e),
         }
@@ -195,28 +185,29 @@ impl BackingStore for FileBackingStore {
 
 #[derive(Debug)]
 struct MemoryEntry {
-    pwhash: String,
+    credentials: String,
     locked: bool,
 }
 
 #[derive(Debug)]
 pub struct MemoryBackingStore {
-    users: HashMap<String, MemoryEntry>,
+    users: Mutex<HashMap<String, MemoryEntry>>,
 }
 
 impl MemoryBackingStore {
     pub fn new() -> MemoryBackingStore {
         MemoryBackingStore {
-            users: HashMap::new(),
+            users: Mutex::new(HashMap::new()),
         }
     }
 }
 
 impl BackingStore for MemoryBackingStore {
-    fn get_pwhash(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError> {
-        match self.users.get(user) {
+    fn get_credentials(&self, user: &str, fail_if_locked: bool) -> Result<String, BackingStoreError> {
+        let hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.get(user) {
             Some(entry) => if !(fail_if_locked && entry.locked) {
-                Ok(entry.pwhash.to_string())
+                Ok(entry.credentials.to_string())
             } else {
                 Err(BackingStoreError::Locked)
             },
@@ -224,12 +215,13 @@ impl BackingStore for MemoryBackingStore {
         }
     }
 
-    fn update_pwhash(&mut self, user: &str, new_pwhash: &str) -> Result<(), BackingStoreError> {
-        match self.users.get_mut(user) {
+    fn update_credentials(&mut self, user: &str, new_creds: &str) -> Result<(), BackingStoreError> {
+        let mut hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.get_mut(user) {
             Some(entry) => match entry.locked {
                 true => Err(BackingStoreError::Locked),
                 false => {
-                    entry.pwhash = new_pwhash.to_string();
+                    entry.credentials = new_creds.to_string();
                     Ok(())
                 },
             },
@@ -238,7 +230,8 @@ impl BackingStore for MemoryBackingStore {
     }
 
     fn lock(&mut self, user: &str) -> Result<(), BackingStoreError> {
-        match self.users.get_mut(user) {
+        let mut hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.get_mut(user) {
             Some(entry) => {
                 entry.locked = true;
                 Ok(())
@@ -248,14 +241,16 @@ impl BackingStore for MemoryBackingStore {
     }
 
     fn islocked(&self, user: &str) -> Result<bool, BackingStoreError> {
-        match self.users.get(user) {
+        let hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.get(user) {
             Some(entry) => Ok(entry.locked),
             None => Err(BackingStoreError::NoSuchUser),
         }
     }
 
     fn unlock(&mut self, user: &str) -> Result<(), BackingStoreError> {
-        match self.users.get_mut(user) {
+        let mut hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.get_mut(user) {
             Some(entry) => {
                 entry.locked = false;
                 Ok(())
@@ -264,18 +259,20 @@ impl BackingStore for MemoryBackingStore {
         }
     }
 
-    fn create(&mut self, user: &str, pwhash: &str) -> Result<(), BackingStoreError> {
-        if self.users.contains_key(user) {
+    fn create(&mut self, user: &str, creds: &str) -> Result<(), BackingStoreError> {
+        let mut hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        if hashmap.contains_key(user) {
             Err(BackingStoreError::UserExists)
         } else {
-            self.users.insert(user.to_string(),
-                MemoryEntry { pwhash: pwhash.to_string(), locked: false, });
+            hashmap.insert(user.to_string(),
+                MemoryEntry { credentials: creds.to_string(), locked: false, });
             Ok(())
         }
     }
 
     fn delete(&mut self, user: &str) -> Result<(), BackingStoreError> {
-        match self.users.remove(user) {
+        let mut hashmap = try!(self.users.lock().map_err(|_| BackingStoreError::Mutex));
+        match hashmap.remove(user) {
             Some(_) => Ok(()),
             None => Err(BackingStoreError::NoSuchUser),
         }
