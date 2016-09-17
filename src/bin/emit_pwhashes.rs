84a5bb573e177d23aa180af3e2498997a434ee31
Ben Stern <bstern@kukaret.com>
2016-09-16 20:22:31 -0400

    Whitespace.

--- src/bin/emit_pwhashes.rs
+++ src/bin/emit_pwhashes.rs
@@ -7,8 +7,8 @@ use pwhash::bcrypt;
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-	let plaintext = line.unwrap();
-	let hash = bcrypt::hash(&plaintext).unwrap();
-	println!("The hash of {} is {}", plaintext, hash);
+        let plaintext = line.unwrap();
+        let hash = bcrypt::hash(&plaintext).unwrap();
+        println!("The hash of {} is {}", plaintext, hash);
     }
 }
